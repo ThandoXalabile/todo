1f177a22aae133141e944dacd85f8ce01c6f4c31
Your Name <you@example.com>
2021-02-09 15:58:03 +0200

    created todo app

---
+++ todo/src/todoadd.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.todoadd(this.state);
+        this.setState({
+            content: ''
+        })
+    }
+    render() {
+        return ( <
+            div >
+            <
+            form onSubmit = { this.handleSubmit } >
+            <
+            label > Click to Add new todo: < /label> <
+            input type = "text"
+            onChange = { this.handleChange }
+            value = { this.state.content }
+            /> < /
+            form > <
+            /div>
+        )
+    }
+}
+
+export default
